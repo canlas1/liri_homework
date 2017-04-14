a3ee98b66f3a8832188ebcf4c8e5ceaf1ce300fc
canlas1 <dc@uscr.com>
2017-04-14 17:34:22 -0400

    adding modules and created functions

--- keys.js
+++ keys.js
@@ -1,18 +1,33 @@
-$(document).ready(function(){
+var input = process.argv;
+console.log(input);
 
-console.log('this is loaded');
+var operator = input[2];
 
-exports.twitterKeys = {
-   consumer_key: 'GTm2LbGmAOwI0WxRPvQPYYbZk',
-   consumer_secret: 'LJbKMPBd6xFsy24ixqEPfH11zBnnAZbSauzPcHVGEcZc0XqISM',
-   access_token_key: '852547974655864832-BrVeA1H25Ekqm5TgOmGzAlUCWowcsrd',
-   access_token_secret: 'E2O7lGX7NbG2jVxUz2z1Z8JZ7BAXs1SgMzYNOMZ4Nthaz',
- }
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: 'GTm2LbGmAOwI0WxRPvQPYYbZk',
+  consumer_secret: 'LJbKMPBd6xFsy24ixqEPfH11zBnnAZbSauzPcHVGEcZc0XqISM',
+  access_token_key: '852547974655864832-BrVeA1H25Ekqm5TgOmGzAlUCWowcsrd',
+  access_token_secret: 'E2O7lGX7NbG2jVxUz2z1Z8JZ7BAXs1SgMzYNOMZ4Nthaz'
+});
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+// exports.twitterKeys = {
+//    consumer_key: 'GTm2LbGmAOwI0WxRPvQPYYbZk',
+//    consumer_secret: 'LJbKMPBd6xFsy24ixqEPfH11zBnnAZbSauzPcHVGEcZc0XqISM',
+//    access_token_key: '852547974655864832-BrVeA1H25Ekqm5TgOmGzAlUCWowcsrd',
+//    access_token_secret: 'E2O7lGX7NbG2jVxUz2z1Z8JZ7BAXs1SgMzYNOMZ4Nthaz',
+//  }
 
 
 
-
-});//End of $(document).ready
 
 
 
--- liri.js
+++ liri.js
@@ -1,52 +1,119 @@
+var stuffINeed = require("./keys.js");
+var fs = require("fs");
 
-var input = process.argv;
-console.log(input);
+console.log(fs);
+console.log(stuffINeed);
 
 var operator = input[2];
-var num1 = input[3];
-var num2 = input[4];
 
-console.log(operator);
-console.log(num1);
-console.log(num2);
+switch (operator) {
+  case "my-tweets":
+    tweets();
+    break;
 
-//The parseFloat() function parses a string and returns a floating point number.
+  case "spotify-this-song":
+    spotify();
+    break;
 
-//This function determines if the first character in the specified string is a number. If it is, it parses the string until it reaches the end of the number, and returns the number as a number, not as a string.
+  case "movie-this":
+    movie();
+    break;
 
-if(operator === 'add'){
-	console.log(parseFloat(num1) + parseFloat(num2));
-
+  case "do-what-it-says":
+    what();
+    break;
 }
 
-else if(operator === 'subtract'){
-	console.log(parseFloat(num1) - parseFloat(num2));
 
+// Store all of the arguments in an array
+var nodeArgs = process.argv;
+
+// Create an empty variable for holding the movie name
+var movieName = "";
+
+
+// Includes the FS package for reading and writing packages
+var fs = require("fs");
+
+// Running the readFile module that's inside of fs.
+// Stores the read information into the variable "data"
+fs.readFile("best_things_ever.txt", "utf8", function(err, data) {
+
+  // Break the string down by comma separation and store the contents into the output array.
+  var output = data.split(",");
+
+  // Loop Through the newly created output array
+  for (var i = 0; i < output.length; i++) {
+
+    // Print each element (item) of the array/
+    console.log(output[i]);
+  }
+
+});
+
+// Loop through all the words in the node argument
+// And do a little for-loop magic to handle the inclusion of "+"s
+for (var i = 2; i < nodeArgs.length; i++) {
+
+  if (i > 2 && i < nodeArgs.length) {
+
+    movieName = movieName + "+" + nodeArgs[i];
+
+  }
+
+  else {
+
+    movieName += nodeArgs[i];
+
+  }
 }
 
-else if(operator === 'multiply'){
-	console.log(parseFloat(num1) * parseFloat(num2));
+
+// var total = input[2];
+// var withdraw = input[3];
+// var deposit = input[4];
+
+// console.log(total);
+// console.log(withdraw);
+// console.log(deposit);
+
+// //The parseFloat() function parses a string and returns a floating point number.
+
+// //This function determines if the first character in the specified string is a number. If it is, it parses the string until it reaches the end of the number, and returns the number as a number, not as a string.
+
+// if(operator === 'add'){
+// 	console.log(parseFloat(num1) + parseFloat(num2));
+
+// }
+
+// else if(operator === 'subtract'){
+// 	console.log(parseFloat(num1) - parseFloat(num2));
+
+// }
+
+// else if(operator === 'multiply'){
+// 	console.log(parseFloat(num1) * parseFloat(num2));
 	
-}
+// }
 
-else if(operator === 'divide'){
-	console.log(parseFloat(num1) / parseFloat(num2));
+// else if(operator === 'divide'){
+// 	console.log(parseFloat(num1) / parseFloat(num2));
 	
-}
+// }
 
-else if(operator === 'remainder'){
-	console.log(parseFloat(num1) % parseFloat(num2));
+// else if(operator === 'remainder'){
+// 	console.log(parseFloat(num1) % parseFloat(num2));
 	
-}
+// }
 
-else if(operator === 'exp'){
-	console.log(parseFloat(num1) ^ parseFloat(num2));
+// else if(operator === 'exp'){
+// 	console.log(parseFloat(num1) ^ parseFloat(num2));
 	
-}
+// }
 
-else{
-	console.log('not found');
-}
+// else{
+// 	console.log('not found');
+// }
 
 
 
